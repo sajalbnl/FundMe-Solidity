3482e853080e057084af9565c5055a2ddafd9350
Sajal Bansal <sajalbnl123@gmail.com>
2025-06-20 12:12:20 +0000

    added more test cases

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -11,8 +11,8 @@ error NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
 
-    mapping(address => uint256) public addressToAmountFunded;
-    address[] public funders;
+    mapping(address => uint256) public s_addressToAmountFunded;
+    address[] public s_funders;
     AggregatorV3Interface private s_priceFeed;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
@@ -29,8 +29,8 @@ contract FundMe {
     function fund() public payable {
         require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
     }
 
     function getVersion() public view returns (uint256) {
@@ -44,11 +44,11 @@ contract FundMe {
     }
 
     function withdraw() public onlyOwner {
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
+        for (uint256 funderIndex = 0; funderIndex < s_funders.length; funderIndex++) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);
+        s_funders = new address[](0);
         // // transfer
         // payable(msg.sender).transfer(address(this).balance);
 
@@ -79,6 +79,15 @@ contract FundMe {
     receive() external payable {
         fund();
     }
+
+    function getAddressToAmountFunded(address funder) external view returns (uint256) {
+        return s_addressToAmountFunded[funder];
+    }
+
+    function getFunder(uint256 index) external view returns (address) {
+        return s_funders[index];
+    }
+
 }
 
 // Concepts we didn't cover yet (will cover in later sections)
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -7,24 +7,52 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 constant AMOUNT = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether; 
+
 
     function setUp() public {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE); // Give USER 10 ETH
     }
 
-    function testMinimumDollarisFive() public {
+    function testMinimumDollarisFive() public view {
         assertEq(fundMe.MINIMUM_USD(),5 * 10 ** 18, "Minimum USD should be 5 ETH");
     }
 
-    function testOwnerIsMsgSender() public {
+    function testOwnerIsMsgSender() public view {
         assertEq(fundMe.i_owner(),msg.sender, "Owner should be the msg.sender");
     }
 
-    function testGetVersion() public {
+    function testGetVersion() public view {
         uint256 version = fundMe.getVersion();
         console.log("Chainlink Version: %s", version);
         assertEq(version, 4, "Chainlink version should be greater than 0");
     }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.prank(USER);
+        
+        fundMe.fund{value: AMOUNT}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded,AMOUNT);  // Sending 1 ETH, which is less than the minimum of 5 ETH
+    }
+
+    function testAddsFunderToArray() public {
+        vm.prank(USER);
+        fundMe.fund{value: AMOUNT}();
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER, "Funder should be added to the funder list");
+    }
+
+    function testOnlyOwnerCanWithdraw() public {
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+
+    
 
 }
